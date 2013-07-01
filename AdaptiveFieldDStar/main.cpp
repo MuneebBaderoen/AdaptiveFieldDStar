27bda042b1e869ba2e2e489748f0338535f9d302
Muneeb Baderoen <mbaderoen@gmail.com>
2013-07-02 01:37:20 +0200

    int main return 0, compiles with cgal includes

---
+++ AdaptiveFieldDStar/main.cpp
@@ -0,0 +1,20 @@
+
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/convex_hull_3.h>
+#include <CGAL/Width_default_traits_3.h>
+#include <CGAL/Width_3.h>
+#include <CGAL/Timer.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+int main(){
+
+
+	return 0;
+}
